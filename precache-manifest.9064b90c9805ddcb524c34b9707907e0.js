e00851e4702c3ab062675a9d553501b00fbef60c
YuriPN <ypedroso8@gmail.com>
2019-03-06 17:37:12 -0400

    Updates

---
+++ precache-manifest.9064b90c9805ddcb524c34b9707907e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/real-time-editor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48fca68d2dc94d97efbc",
+    "url": "/real-time-editor/static/js/runtime~main.48fca68d.js"
+  },
+  {
+    "revision": "e4a9df1691921023a65a",
+    "url": "/real-time-editor/static/js/main.e4a9df16.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/real-time-editor/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "e4a9df1691921023a65a",
+    "url": "/real-time-editor/static/css/main.22435fcb.chunk.css"
+  },
+  {
+    "revision": "954491df19a77a36af2761395c5ca7ef",
+    "url": "/real-time-editor/index.html"
+  }
+];
